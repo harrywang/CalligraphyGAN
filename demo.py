fcd9eadd3e191b3e98d7fbe75eab90fe8d832d3d
Jinggang Zhuo <zhuojg1519@outlook.com>
2020-01-19 13:50:50 +0800

    rewrite some functions and update gui demo

--- demo.py
+++ demo.py
@@ -1,30 +1,27 @@
 # -*- coding: utf-8 -*-
-from model import CGAN
-from denoise import *
 import os
-from bert_client import BertClientQuery
-from style_transfer import Stylizer
-import datetime
 import time
 import platform
 import subprocess
 import inquirer
 import numpy as np
-import PIL, glob
+import glob
 from PIL import Image
 from math import ceil, floor
+from ai_menu import AIMenu
+from oil_painting import OilPaint
 
 
 words = ['且', '世', '东', '九', '亭', '今', '从', '令', '作', '使',
-        '侯', '元', '光', '利', '印', '去', '受', '右', '司', '合',
-        '名', '周', '命', '和', '唯', '堂', '士', '多', '夜', '奉',
-        '女', '好', '始', '字', '孝', '守', '宗', '官', '定', '宜',
-        '室', '家', '寒', '左', '常', '建', '徐', '御', '必', '思',
-        '意', '我', '敬', '新', '易', '春', '更', '朝', '李', '来',
-        '林', '正', '武', '氏', '永', '流', '海', '深', '清', '游',
-        '父', '物', '玉', '用', '申', '白', '皇', '益', '福', '秋',
-        '立', '章', '老', '臣', '良', '莫', '虎', '衣', '西', '起',
-        '足', '身', '通', '遂', '重', '陵', '雨', '高', '黄', '鼎']
+         '侯', '元', '光', '利', '印', '去', '受', '右', '司', '合',
+         '名', '周', '命', '和', '唯', '堂', '士', '多', '夜', '奉',
+         '女', '好', '始', '字', '孝', '守', '宗', '官', '定', '宜',
+         '室', '家', '寒', '左', '常', '建', '徐', '御', '必', '思',
+         '意', '我', '敬', '新', '易', '春', '更', '朝', '李', '来',
+         '林', '正', '武', '氏', '永', '流', '海', '深', '清', '游',
+         '父', '物', '玉', '用', '申', '白', '皇', '益', '福', '秋',
+         '立', '章', '老', '臣', '良', '莫', '虎', '衣', '西', '起',
+         '足', '身', '通', '遂', '重', '陵', '雨', '高', '黄', '鼎']
 
 
 def open_file(path):
@@ -38,115 +35,101 @@ def open_file(path):
 
 if __name__ == '__main__':
     print('initializing ......')
-    bcq = BertClientQuery(words, topk=10)
-
-    cgan = CGAN()
-    cgan.reload('./ckpt')
-    stylizer = Stylizer()
+    menu = AIMenu(result_path='./result', topk=10)
 
     while True:
         query = input("Enter dish name - [default:红烧肉卤鸡蛋]:")
-        if len(query) == 0 :
+        if len(query) == 0:
             query = "红烧肉卤鸡蛋"
 
-        topk_idx = bcq.query(query)
-        print(topk_idx)
+        # topk_idx = bcq.query(query)
+        # print(topk_idx)
 
         time_now = str(int(time.time()))
         charactor_file = './result/%s.png' % time_now
         resized_file = './result/%s_convert.png' % time_now
         styled_file = './result/%s_stylized.png' % time_now
         final_output_file = './result/%s_final.jpg' % time_now
-
+        oil_file = './result/%s_oil.png' % time_now
+        oil_dir = './result/%s_oil' % time_now
 
         try:
             # generate a character
-            print('generating the character...')
-            cgan.generate_one_image(topk_idx[5:], result_path=charactor_file)
 
-            # resize and denoise
-            print('resizing and denoising the character...')
+            # TODO: inquirer is not supported on Windows
+            # styles = [
+            #     inquirer.List('style',
+            #                   message="Choose a style:",
+            #                   choices=['Picasso', 'Pollock', 'Rousseau', 'Rothko', 'deKooning'],
+            #                   ),
+            # ]
 
-            #resize(charactor_file, (1200, 1200), resized_file)
+            # chosen_style = inquirer.prompt(styles)
+            # style_img_file = './style_image/%s.jpg' % chosen_style['style'].lower()
+            style_img_file = './style_image/Pollock.jpg'
 
-            # denoising will change the style transfer effect to the background
-            resize_and_denoise(charactor_file, (1200, 1200), resized_file)
+            img, resized_img, denoised_img, stylized_img = menu.generate(query, style_img_file=style_img_file)
 
+            Image.fromarray(img).convert('RGB').save(charactor_file)
+            Image.fromarray(denoised_img.astype(np.uint8)).save(resized_file)
+            Image.fromarray(stylized_img.astype(np.uint8)).save(styled_file)
 
+            print('start oil painting...')
+            if not os.path.exists(oil_dir):
+                os.makedirs(oil_dir)
+            op = OilPaint(file_name=resized_file)
+            oil_img = op.paint(epoch=30, batch_size=64, result_dir=oil_dir)
 
-            # change style_image_path to generate different style!
-            # if something wrong with style_image_path, it will use default style image
-
-            styles = [
-                inquirer.List('style',
-                              message="Choose a style:",
-                              choices=['Picasso', 'Pollock', 'Rousseau', 'Rothko', 'deKooning'],
-                          ),
-            ]
-
-            chosen_style = inquirer.prompt(styles)
-            style_img_file = './style_image/%s.jpg' % chosen_style['style'].lower()
-
-            print('applying style transfer...')
-            stylizer.transfer(style_image_path=style_img_file,
-                                   content_image_path=resized_file,
-                                   output_size=1200,
-                                   result_path=styled_file
-                                   )
-
+            Image.fromarray(oil_img.astype('uint8')).save(oil_file)
 
             # combine the output images into one
             print('Finishing up...')
             frame_width = 1920
-            images_per_row = 4
+            images_per_row = 5
             padding = 2
 
             images = glob.glob('./result/%s*.png' % time_now)
             images.append(style_img_file)
             images.sort()
-            print(images)
 
             img_width, img_height = Image.open(images[0]).size
-            sf = (frame_width-(images_per_row-1)*padding)/(images_per_row*img_width)       #scaling factor
-            scaled_img_width = ceil(img_width*sf)                   #s
-            scaled_img_height = ceil(img_height*sf)
+            sf = (frame_width - (images_per_row - 1) * padding) / (images_per_row * img_width)  # scaling factor
+            scaled_img_width = ceil(img_width * sf)  # s
+            scaled_img_height = ceil(img_height * sf)
 
-            number_of_rows = ceil(len(images)/images_per_row)
-            frame_height = ceil(sf*img_height*number_of_rows)
+            number_of_rows = ceil(len(images) / images_per_row)
+            frame_height = ceil(sf * img_height * number_of_rows)
 
             new_im = Image.new('RGB', (frame_width, frame_height))
 
-            i,j=0,0
+            i, j = 0, 0
             for num, im in enumerate(images):
-                if num%images_per_row==0:
-                    i=0
+                if num % images_per_row == 0:
+                    i = 0
                 im = Image.open(im)
-                #Here I resize my opened image, so it is no bigger than 100,100
-                im.thumbnail((scaled_img_width,scaled_img_height))
-                #Iterate through a 4 by 4 grid with 100 spacing, to place my image
-                y_cord = (j//images_per_row)*scaled_img_height
-                new_im.paste(im, (i,y_cord))
-                #print(i, y_cord)
-                i=(i+scaled_img_width)+padding
-                j+=1
+                # Here I resize my opened image, so it is no bigger than 100,100
+                im.thumbnail((scaled_img_width, scaled_img_height))
+                # Iterate through a 5 by 5 grid with 100 spacing, to place my image
+                y_cord = (j // images_per_row) * scaled_img_height
+                new_im.paste(im, (i, y_cord))
+                # print(i, y_cord)
+                i = (i + scaled_img_width) + padding
+                j += 1
 
             new_im.save(final_output_file, "JPEG", quality=80, optimize=True, progressive=True)
-            #new_im.show()
 
-            # open the final combined file
-            open_file(final_output_file)
+            # TODO: inquirer is not supported on Windows
+            # try_again_prompt = [
+            #     inquirer.List('try',
+            #                   message="Try again?",
+            #                   choices=['Yes', 'No'],
+            #                   ),
+            # ]
 
-            try_again_prompt = [
-                inquirer.List('try',
-                              message="Try again?",
-                              choices=['Yes', 'No'],
-                          ),
-            ]
-
-            answer = inquirer.prompt(try_again_prompt)
-            if answer['try']=="No":
-                print('bye!!')
-                break
+            # answer = inquirer.prompt(try_again_prompt)
+            # if answer['try'] == "No":
+            #     print('bye!!')
+            #     break
 
         except Exception as e:
             print(e)
--- style_transfer.py
+++ style_transfer.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, division, print_function
 
-import functools
 import os
 
 from matplotlib import gridspec
@@ -10,10 +9,10 @@ import tensorflow as tf
 import tensorflow_hub as hub
 from PIL import Image
 
+
 class Stylizer:
     def __init__(self):
         # Pay attention. Bad network or GreatWall may cause failure.
-        # print('Load TF-Hub module.')
         os.environ["TFHUB_CACHE_DIR"] = './ckpt/tfhub'
         hub_handle = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
         self.hub_module = hub.load(hub_handle)
@@ -22,14 +21,15 @@ class Stylizer:
     # Define image loading and visualization functions  { display-mode: "form" }
     # Returns a cropped square image.
     def crop_center(self, image):
-      shape = image.shape
-      new_shape = min(shape[1], shape[2])
-      offset_y = max(shape[1] - shape[2], 0) // 2
-      offset_x = max(shape[2] - shape[1], 0) // 2
-      image = tf.image.crop_to_bounding_box(
-          image, offset_y, offset_x, new_shape, new_shape)
+        shape = image.shape
+        new_shape = min(shape[1], shape[2])
+        offset_y = max(shape[1] - shape[2], 0) // 2
+        offset_x = max(shape[2] - shape[1], 0) // 2
+        image = tf.image.crop_to_bounding_box(
+            image, offset_y, offset_x, new_shape, new_shape
+        )
 
-      return image
+        return image
 
     def load_image(self, image_url, image_size=(256, 256), preserve_aspect_ratio=True):
         """Loads and preprocesses images."""
@@ -50,7 +50,7 @@ class Stylizer:
         n = len(images)
         image_sizes = [image.shape[1] for image in images]
         w = (image_sizes[0] * 6) // 320
-        plt.figure(figsize=(w  * n, w))
+        plt.figure(figsize=(w * n, w))
         gs = gridspec.GridSpec(1, n, width_ratios=image_sizes)
         for i in range(n):
             plt.subplot(gs[i])
@@ -59,8 +59,8 @@ class Stylizer:
             plt.title(titles[i] if len(titles) > i else '')
         plt.show()
 
-    def import_image(self, img_path, image_size=(256, 256), preserve_aspect_ratio=True):
-        img = plt.imread(img_path).astype(np.float32)[np.newaxis, ...]
+    def import_image(self, img, image_size=(256, 256)):
+        img = img.astype(np.float32)[np.newaxis, ...]
         # print(img.shape)
         # img = img[:, :, :, :3]
         if img.max() > 1.0:
@@ -72,28 +72,27 @@ class Stylizer:
 
         return img
 
-    def transfer(self, style_image_path, content_image_path, output_size, result_path):
-        # Load example images
+    def transfer(self, content_image, output_size, style_image_path, style_img_size=(256, 256)):
+        # The style prediction model was trained with image size 256 and it's the
+        # recommended image size for the style image (though, other sizes work as
+        # well but will lead to different results).
+        self.style_img_size = style_img_size  # Recommended to keep it at 256.
+
+        try:
+            img = np.array(Image.open(style_image_path))
+            self.style_image = self.import_image(img, style_img_size)
+        except Exception as e:
+            print(e)
+            style_image_url = 'https://user-images.githubusercontent.com/595772/66951165-cc179880-f027-11e9-8e46-407defc5c2a4.jpg'
+            self.style_image = self.load_image(style_image_url, style_img_size)
 
         output_image_size = output_size
 
         # The content image size can be arbitrary.
         content_img_size = (output_image_size, output_image_size)
-        # The style prediction model was trained with image size 256 and it's the
-        # recommended image size for the style image (though, other sizes work as
-        # well but will lead to different results).
-        style_img_size = (256, 256)  # Recommended to keep it at 256.
 
-        try:
-            style_image = self.import_image(style_image_path, style_img_size)
-        except Exception as e:
-            print(e)
-            style_image_url = 'https://user-images.githubusercontent.com/595772/66951165-cc179880-f027-11e9-8e46-407defc5c2a4.jpg'
-            style_image = self.load_image(style_image_url, style_img_size)
-
-        content_image = self.import_image(content_image_path, content_img_size)
-        style_image = tf.nn.avg_pool(style_image, ksize=[3, 3], strides=[1, 1], padding='SAME')
-        # show_n([content_image, style_image], ['Content image', 'Style image'])
+        content_image = self.import_image(content_image, content_img_size)
+        style_image = tf.nn.avg_pool(self.style_image, ksize=[3, 3], strides=[1, 1], padding='SAME')
 
         # Stylize content image with given style image.
         # This is pretty fast within a few milliseconds on a GPU.
@@ -103,11 +102,5 @@ class Stylizer:
         stylized_image = np.array(stylized_image)
         stylized_image = np.reshape(stylized_image, (1200, 1200, 3))
         stylized_image *= 255
-        img = Image.fromarray(stylized_image.astype(np.uint8))
-        img.save(result_path)
 
-
-if __name__ == '__main__':
-    stylizer = Stylizer()
-    stylizer.transfer('', './result/1571140502_convert.png', 1200,
-                      result_path='./result/1571140502_convert_style.png')
+        return stylized_image.astype('uint8')
